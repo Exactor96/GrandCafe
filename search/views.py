7a2b65311ef44aea981818da4c37c34dfd92b2d3
Exactor96 <kyzya96@gmail.com>
2019-06-08 15:38:36 +0300

    test search with search by category and ingredients

--- search/views.py
+++ search/views.py
@@ -10,11 +10,19 @@ class ESearchView(View):
 
     def get(self, request, *args, **kwargs):
         context = {}
-
         question = request.GET.get('q')
+        select = request.GET.get('s')
+        print(select)
         if question is not None:
-            search_posts = Post.objects.filter(title__contains=question)
-
+            if select == None:
+                search_posts = Post.objects.filter(title__contains=question)
+            else:
+                if select == 'Название':
+                    search_posts = Post.objects.filter(title__contains=question)
+                if select == 'Ингредиенты':
+                    search_posts = Post.objects.filter(ingredients__contains=question)
+                if select == 'Категория':
+                    search_posts = Post.objects.filter(category__name=question)
             # forming a URL string that will contain the last request.
             # This is important for correct operation of pagination
             context['last_question'] = '?q=%s' % question

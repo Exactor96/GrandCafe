9c0320b1dd3c4a2d73859323679ffde7166c395a
Exactor96 <kyzya96@gmail.com>
2019-06-15 22:36:34 +0300

    make static files work and be great again

--- GrandCafe/urls.py
+++ GrandCafe/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from post import views
+
 urlpatterns = [
 #site root
     path('admin/', admin.site.urls),
